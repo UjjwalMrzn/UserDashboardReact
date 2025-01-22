8c08e6821861e23d87815671a58fd7546055afce
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2025-01-22 11:06:31 +0545

    welcom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Welcome from './components/Welcome'
 
 
 
@@ -12,6 +13,8 @@ const App = () => {
   return(
     <div>
    <Header/>
+      <Welcome name = "Ujjwal"/>
+   <Welcome name = "Ujjwal Maharjan"/>
    <Footer/>
 
     </div>
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,8 @@
+const Welcome = (props) =>{
+    return(
+      <h4>Hello , {props.name}</h4>
+
+    )
+}
+   
+export default Welcome
